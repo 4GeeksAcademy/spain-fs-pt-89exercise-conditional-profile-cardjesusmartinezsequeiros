21c8474f6775ebd083f45aa6bac71dea716486b7
Jesús Martínez Sequeiros <165663442+Jesus2508@users.noreply.github.com>
2025-01-13 11:47:04 +0000

    terminado

--- src/js/app.js
+++ src/js/app.js
@@ -1,54 +1,79 @@
 import "../style/index.css";
-
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("These are the current variables: ", variables);
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
+  let socialMediaIcons = "";
+  if (variables.twitter) {
+    socialMediaIcons += `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`;
+  }
+  if (variables.github) {
+    socialMediaIcons += `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`;
+  }
+  if (variables.linkedin) {
+    socialMediaIcons += `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`;
+  }
+  if (variables.instagram) {
+    socialMediaIcons += `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`;
+  }
+
+  let fullName = `${variables.name ? variables.name : "Your name"} ${
+    variables.lastName ? variables.lastName : "Your lastname"
+  }`;
+  let role = variables.role ? variables.role : "Web Developer";
+  let location = `${variables.city ? variables.city : "City"}, ${
+    variables.country ? variables.country : "Country"
+  }`;
+
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  ${cover}
+<img src="${variables.avatarURL}" class="photo" />
+<h1>${fullName}</h1>
+<h2>${role}</h2>
+<h3>${location}</h3>
+<ul class="${variables.socialMediaPosition}">
+  ${socialMediaIcons}
+</ul>
+</div>
+`;
 }
 
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
+window.onload = function() {
+  window.variables = {
+    includeCover: true,
+    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    socialMediaPosition: "position-right",
+    twitter: null,
+    github: null,
+    linkedin: null,
+    instagram: null,
+    name: null,
+    lastName: null,
+    role: null,
+    country: null,
+    city: null
+  };
+  render(window.variables);
+
+  document.querySelectorAll(".picker").forEach(function(elm) {
+    elm.addEventListener("change", function(e) {
+      const attribute = e.target.getAttribute("for");
+      let values = {};
+      values[attribute] =
+        this.value == "" || this.value == "null"
+          ? null
+          : this.value == "true"
+          ? true
+          : this.value == "false"
+          ? false
+          : this.value;
+      render(Object.assign(window.variables, values));
+    });
+  });
+};
+
 window.onload = function() {
   window.variables = {
     // if includeCover is true the algorithm should show the cover image
